116a9a8fbfbd5d83f7f78738282795bd9938f725
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2021-03-03 16:22:01 +0900

    learned class

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
-interface Human {
-    name : string,
-    age : number,
-    isMale : boolean
+class Human {
+    public name : string;
+    public age : number;
+    public isMale : boolean;
+    constructor(name:string, age:number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.isMale = gender === "male";
+    }
 }
 
-const person = {
-    name : "aroundthistime",
-    age : 25,
-    isMale : true
-}
+// interface Human {
+//     name : string,
+//     age : number,
+//     isMale : boolean
+// }
+
+const me = new Human("aroundthistime", 20, "male");
 
 const sayHi = (person : Human):boolean => {
     console.log(`Hello ${person.name}! - ${person.age}, you are ${person.isMale ? "man" : "woman"}`);
     return true;
 }
 
-sayHi(person);
+sayHi(me);
 
 export {};
